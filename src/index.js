0ff25564f2614ac9c997505939f397d3db339197
Nasir Hussain <nasirhussainm14@gmail.com>
2019-01-24 22:20:18 +0500

    Implemented Browser Router

--- src/index.js
+++ src/index.js
@@ -17,7 +17,11 @@ import { BrowserRouter } from 'react-router-dom';
 
 // const greeting = <h1>Hello World! | Current Date {getCurrentDate()}</h1>;
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<BrowserRouter>
+<App />
+</BrowserRouter>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
